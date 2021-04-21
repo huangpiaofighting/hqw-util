5fab823c2eb990325c938f8f7b95426fe3856748
huangpiao <1149628750@qq.com>
2021-04-21 20:05:23 +0800

    hqw

--- Test/index.js
+++ Test/index.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { getWeather } from "@/weather";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { getWeather } from '@/weather';
 
 function Test() {
-  const [city, setCity] = useState("test");
-  getWeather().then(res => {
+  const [city, setCity] = useState('test');
+  getWeather().then((res) => {
     setTimeout(() => {
-      console.log("res.city", res.city);
+      console.log('res.city', res.city);
       setCity(res.city);
     }, 3000);
   });
   return (
     <>
-      <div>{city}</div>
+      <div>
+        {city}
+        {'thisis city'}
+      </div>
     </>
   );
 }
 
-ReactDOM.render(<Test />, document.getElementById("root"));
+ReactDOM.render(<Test />, document.getElementById('root'));
